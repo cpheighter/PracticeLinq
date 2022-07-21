1d782a912c83455a0cf41cd0287f72d66aebbd03
cpheighter <cheighterjr@gmail.com>
2022-07-21 11:45:08 -0400

    init

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Linq;
+
+List<string> videoGames = new List<string>() { "NBA2k", "Minecraft", "Mario Kart", "FIFA", "Madden" };
+videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
